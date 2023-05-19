49632694eefc7d0c157d8cb6255119053d55a687
juliarmn <julia.rm.nascimento@unesp.br>
2023-05-19 17:09:56 -0300

    Acaba com a lista

--- src/aulaheranca/Lista/ExerciciosExtras/exercicio5/Principal.java
+++ src/aulaheranca/Lista/ExerciciosExtras/exercicio5/Principal.java
@@ -4,8 +4,8 @@ public class Principal {
     public static void main(String[] args) {
         Funcionario[] funcionarios = new Funcionario[3];
 //        funcionarios[0] = new Gerente(" Maria ", 5000, 1000);
-        funcionarios[1] = new Vendedor("Jo ̃ao", 3000, 500);
-        funcionarios[2] = new Vendedor(" Lucas ", 2500, 400);
+//        funcionarios[1] = new Vendedor("Jo ̃ao", 3000, 500);
+//        funcionarios[2] = new Vendedor(" Lucas ", 2500, 400);
 //        funcionarios[3] = new Funcionario(" Lucas ", 2500, 400);
 
         for (Funcionario funcionario : funcionarios) {
